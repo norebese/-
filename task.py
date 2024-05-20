8c25920c8afc19e5160e81e9e8aeb88aed7cb717
norebese <ehddls1029@naver.com>
2024-05-20 15:14:12 +0900

    sd

--- task.py
+++ task.py
@@ -1,1 +1,19 @@
-print('hi')
+import requests
+from bs4 import BeautifulSoup
+chart_range = ['1', '2', '3']
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+
+startNum = 1
+for num in chart_range:
+    site = f'https://genie.co.kr/chart/top200?ditc=D&ymd=20240520&hh=12&rtm=Y&pg={num}'
+    request = requests.get(site, headers=header)
+    # print(request)
+    soup = BeautifulSoup(request.text, 'html.parser')
+    titles = soup.findAll('a', {'class': 'title ellipsis'})
+    artist = soup.findAll('a', {'class': 'artist ellipsis'})
+
+    for i, (t,a) in enumerate(zip(titles, artist)):
+        title = t.text.strip()
+        artist = a.text.strip().split('\n')[0]
+        print(f'{i+startNum}위. {title} - {artist}')
+    startNum += 50
